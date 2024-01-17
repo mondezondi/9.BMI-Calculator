ea3b44da7b0964968d4bff20cabea0cfcfcf5899
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-17 12:21:13 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim height, weight, bmi As Double
+        weight = Val(TextBox1.Text)
+        height = Val(TextBox2.Text)
+
+        bmi = weight / (height) ^ 2
+
+        TextBox3.Text = bmi
+
+        If bmi < 18 Then
+            TextBox4.Text = "You are underweight"
+        ElseIf 18 <= bmi And bmi < 26 Then
+            TextBox4.Text = "You are normal"
+        Else
+            TextBox4.Text = "You are overweight"
+        End If
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
